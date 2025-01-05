b1eb99a8d7766841978c0dd742520ce53b88282d
Eemeli Kukkonen <kukkoneneemeli@gmail.com>
2025-01-05 17:43:28 +0200

    remove "data"-field from startup command

--- Godot/addons/neuro-sdk/messages/outgoing/startup.gd
+++ Godot/addons/neuro-sdk/messages/outgoing/startup.gd
@@ -3,3 +3,8 @@ extends OutgoingMessage
 
 func _get_command() -> String:
 	return "startup"
+
+func get_ws_message() -> WsMessage:
+	var ws_message: WsMessage = super()
+	ws_message.data = null
+	return ws_message
