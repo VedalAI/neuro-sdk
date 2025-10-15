2c06e9748e42cd8a3ba807eab6058fbaca1f830a
alexvoid <32238504+Alexejhero@users.noreply.github.com>
2025-10-16 00:59:42 +0300

    2.0.0-pre.4

--- Unity/Assets/Json/JTokenWrapperExtensions.cs
+++ Unity/Assets/Json/JTokenWrapperExtensions.cs
@@ -8,10 +8,16 @@ namespace NeuroSdk.Json;
 
 public static class JTokenWrapperExtensions
 {
-    public static T? GetValue<T>(this IJTokenWrapper data, string key, T? defaultValue = default)
+    public static T? GetValue<T>(this IJTokenWrapper data, string key, T? defaultValue)
     {
         JToken? jToken = data.Data?[key];
         return jToken != null ? jToken.Value<T>() : defaultValue;
+    }
+
+    public static T? GetValue<T>(this IJTokenWrapper data, string key) where T : class
+    {
+        JToken? jToken = data.Data?[key];
+        return jToken?.Value<T>();
     }
 
     public static IEnumerable<T?> GetArray<T>(this IJTokenWrapper data, string key)
