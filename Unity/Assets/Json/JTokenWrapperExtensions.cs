1e9795192f99f8ae0fc5061643da1d49c39fb3b2
Pasu4 <pascalwieckhorst02@gmail.com>
2025-12-20 18:06:18 +0100

    Change file-scoped namespaces to normal namespaces
    Unity refuses to compile it otherwise

--- Unity/Assets/Json/JTokenWrapperExtensions.cs
+++ Unity/Assets/Json/JTokenWrapperExtensions.cs
@@ -4,24 +4,25 @@ using System.Collections.Generic;
 using System.Linq;
 using Newtonsoft.Json.Linq;
 
-namespace NeuroSdk.Json;
-
-public static class JTokenWrapperExtensions
+namespace NeuroSdk.Json
 {
-    public static T? GetValue<T>(this IJTokenWrapper data, string key, T? defaultValue)
+    public static class JTokenWrapperExtensions
     {
-        JToken? jToken = data.Data?[key];
-        return jToken != null ? jToken.Value<T>() : defaultValue;
-    }
+        public static T? GetValue<T>(this IJTokenWrapper data, string key, T? defaultValue)
+        {
+            JToken? jToken = data.Data?[key];
+            return jToken != null ? jToken.Value<T>() : defaultValue;
+        }
 
-    public static T? GetValue<T>(this IJTokenWrapper data, string key) where T : class
-    {
-        JToken? jToken = data.Data?[key];
-        return jToken?.Value<T>();
-    }
+        public static T? GetValue<T>(this IJTokenWrapper data, string key) where T : class
+        {
+            JToken? jToken = data.Data?[key];
+            return jToken?.Value<T>();
+        }
 
-    public static IEnumerable<T?> GetArray<T>(this IJTokenWrapper data, string key)
-    {
-        return data.Data?[key]?.Values<T?>() ?? Enumerable.Empty<T?>();
+        public static IEnumerable<T?> GetArray<T>(this IJTokenWrapper data, string key)
+        {
+            return data.Data?[key]?.Values<T?>() ?? Enumerable.Empty<T?>();
+        }
     }
 }
